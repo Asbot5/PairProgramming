b2fb7473c5e16118b99712206c33f894c65c8999
Asmus Skafte Böttcher <asmu0543@edu.easj.dk>
2021-11-01 11:04:25 +0100

    MusicRecord class

---
+++ Models/MusicRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PairProgramming.Models
+{
+    public class MusicRecord
+    {
+        public string Title { get; set; }
+    }
+}
